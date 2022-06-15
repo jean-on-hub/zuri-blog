f32478b55876393c613b793da8150a23e8d7a9cd
jean-on-hub <jean.eudes@amalitech.org>
2022-06-15 14:26:35 +0000

    blog model

---
+++ Jeaneudes/blog/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+# Create your models here.
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+    Author = get_user_model()
+    Published_date = models.DateTimeField()
